8c6e99639df2e66679fb4983ec7bad4cb484aa32
furkan <furkan@LAPTOP-7AKVO154>
2023-10-08 21:57:41 +0200

    Add project files.

---
+++ afleveringlib/BooksRepository.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OblikatoriskAflevering
+{
+    public class BooksRepository
+    {
+        private List<Book> _books = new List<Book>();
+        public BooksRepository()
+        {
+            _books = new List<Book>();
+            _books.Add(new Book() { Id = 5, Title = "Hunde",Price = 25});
+            _books.Add(new Book() { Id = 6, Title = "Katte", Price = 288 });
+            _books.Add(new Book() { Id = 7, Title = "Fugle", Price = 177 });
+            _books.Add(new Book() { Id = 8, Title = "Fisk", Price = 69 });
+            _books.Add(new Book() { Id = 9, Title = "Aber", Price = 311 });
+
+        }
+        public IEnumerable<Book>Get(int? fillprice = null, int? sortprice = null)
+        {
+            IEnumerable<Book> result = new List<Book>(_books);
+            if(fillprice!= null)
+            {
+                result = result.Where(a=> a.Price > fillprice);
+            }
+            return result;
+
+        }
+        public List<Book> GetallBook()
+        {
+            return new List<Book>(_books);
+        }
+        public Book Add(Book book)
+        {
+            book.validate();
+            _books.Add(book);
+            return book;
+        }
+        public Book Getbyid(int id)
+        {
+            foreach(Book book in _books)
+            {
+                if (book.Id == id)
+                {
+                    return book;
+                }
+            }
+            return null;
+        }
+        public Book Deelete(int id)
+        {
+            Book delete = Getbyid(id);
+            if (delete != null)
+            {
+                _books.Remove(delete);
+                return delete;
+            }
+            return null;
+        }
+        public Book Update(int id, Book book)
+        {
+            book.validate();
+            foreach(Book abokk in _books)
+            {
+                if(abokk.Id == id)
+                {
+                    abokk.Id = book.Id;
+                    abokk.Title = book.Title;
+                    abokk.Price = book.Price;
+                    return abokk;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ afleveringlibTests/BookTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OblikatoriskAflevering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace OblikatoriskAflevering.Tests
+{
+    [TestClass()]
+    public class BookTests
+    {
+        private Book bookOne = new Book { Id= 1, Title= "Furkan", Price= 120 };
+        private Book bookTwo = new Book { Id = 2, Title = "Dirican", Price = 1120 };
+        private Book bookThree = new Book { Id = 3, Title = "Zealand", Price = 1333 };
+        private Book bookFour = new Book { Id = 4, Title = "Roskilde", Price = -2 };
+        private Book bookFive = new Book { Id = 5, Title = "Ko", Price = 220 };
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            string str = bookOne.ToString();
+            Assert.AreEqual("{Id=1, Title=Furkan, Price=120}", str);
+        }
+
+        [TestMethod()]
+        public void validationTitleTest()
+        {
+            bookOne.validationTitle();
+            Assert.ThrowsException<ArgumentException>(() => bookFive.validationTitle());
+        }
+
+        [TestMethod()]
+        public void validationPriceTest()
+        {
+            bookOne.validationPrice();
+            Assert.ThrowsException<ArgumentException>(() => bookThree.validationPrice());
+            Assert.ThrowsException<ArgumentException>(() => bookFour.validationPrice());
+
+
+        }
+        [TestMethod()]
+        public void validateTest()
+        {
+            bookOne.validate();
+        }
+    }
+}
